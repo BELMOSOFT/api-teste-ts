1812c281930ead81fcd3c36e7bdc7525ea5e458e
Moises Vilas Boas <moisesboas@gmail.com>
2023-05-18 10:37:21 -0300

    feat: all

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+const app: Express = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/ping", (req: Request, res: Response) => {
+  return res.json({
+    message: "pong",
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
