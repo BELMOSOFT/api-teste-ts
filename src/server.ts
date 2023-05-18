528d81377d2aee32ec44f50edf70b94283b52c01
Moises Vilas Boas <moisesboas@gmail.com>
2023-05-18 10:45:37 -0300

    feat: all

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 
 const app: Express = express();
-const port = 4000;
+const port = 80;
 
 app.use(cors());
 app.use(express.json());
